09dde14c00d0dc50e01378b449be84181f7488cc
Hereicus <calimomercan@gmail.com>
2024-05-31 00:20:48 +0300

    Updated category section

---
+++ Vtest94/ViewComponents/HeaderCategoriesViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Vtest94.Data;
+
+namespace Vtest94.ViewComponents
+{
+    public class HeaderCategoriesViewComponent : ViewComponent
+    {
+        private readonly AppDbContext _context;
+
+        public HeaderCategoriesViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var categories = await _context.Categories
+            .Select(c => new
+            {
+                Id = c.Id,
+                Name = c.Name,
+                VideoCount = c.Videos.Count
+            })
+            .OrderBy(c => c.Name)
+            .ToListAsync();
+
+            return View(categories);
+        }
+    }
+}
